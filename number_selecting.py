933be047ee2539ba58aafe151fadfd3e65b6e29e
Emmanuel Enenta <157570024+chinedu-2002@users.noreply.github.com>
2024-06-14 16:04:09 -0500

    Create number_selecting.py

---
+++ number_selecting.py
@@ -0,0 +1,34 @@
+num = int(input())
+if num == 1:
+    print("Drew an Ace")
+elif num == 2:
+    print("Drew a 2")
+elif num == 3:
+    print("Drew a 3")
+elif num == 4:
+    print("Drew a 4")
+elif num == 5:
+    print("Drew a 5")
+elif num == 6:
+    print("Drew a 6")
+elif num == 7:
+    print("Drew a 7")
+elif num == 8:
+    print("Drew an 8")
+elif num == 9:
+    print("Drew a 9")
+elif num == 10:
+    print("Drew a 10")
+elif num == 11:
+    print("Drew a Jack")
+elif num == 12:
+    print("Drew a Queen")
+elif num == 13:
+    print("Drew a King")
+else:
+    print("BAD CARD")
+
+    
+        
+
+     
